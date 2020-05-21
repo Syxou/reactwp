f0eb8f2b244d668eb3e9532a0bd70de46777a965
Illia <syxou27@gmail.com>
2020-05-21 18:22:24 +0200

    fech, update website settings [fe,be]
    front: creating a selection of data from the server, editing the input data, sending data from the input data to the server
    back: update only modified data from the front.

--- reactwp-be/src/components/global/global.js
+++ reactwp-be/src/components/global/global.js
@@ -6,10 +6,14 @@ const Global = require('../../models/global')
 
 router.get('/all', async (req, res, next) => {
     try {
-
-        const global = await Global.query();
-        res.json(global)
-
+        let globalSettings = {}
+        const global = await Global.query()
+        global.forEach((g) => {
+            globalSettings = {
+                ...globalSettings, [g.option_name]: { option_value: g.option_value }
+            }
+        })
+        res.json(globalSettings)
     } catch (error) {
         console.log(error)
         res.status('500')
@@ -17,5 +21,22 @@ router.get('/all', async (req, res, next) => {
     }
 })
 
+router.post('/save', async (req, res, next) => {
+    const data = Object.entries(req.body)
+    try {
+        data.forEach(async d => {
+            const update = await Global.query()
+                .patch({ option_value: d[1] })
+                .where('option_name', '=', d[0])
+                .where('option_value', '!=', d[1])
+            console.log(update)
+        })
+        res.send({ error: false, message: "Updated! 🔥" })
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({ error: true, message: "Error 500 ⛈" })
+        next()
+    }
+})
 
 module.exports = router
--- reactwp-fe/src/containers/settings/GlobalSettings.js
+++ reactwp-fe/src/containers/settings/GlobalSettings.js
@@ -1,18 +1,74 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Button, Input, message } from 'antd';
+import Cookies from 'js-cookie'
+import axios from 'axios'
+
 import Card from '../../compontnts/card/Card'
-import { Input } from 'antd';
+
 
 const { TextArea } = Input;
 
 export default function GlobalSettings() {
 
+    let [siteUrl, setSiteUrl] = useState('')
+    let [homeUrl, setHomeUrl] = useState('')
+    let [aminUrl, setAminUrl] = useState('')
+    let [siteName, setSiteName] = useState('')
+    let [siteDesc, setSiteDesc] = useState('')
+    let [save, setSave] = useState(false)
+
+    useEffect(() => {
+        axios({
+            method: 'get',
+            url: `/admin/api/global/all`,
+            headers: {
+                'Authorization': 'Bearer ' + Cookies.get('token'),
+            }
+        }).then(res => {
+            console.log(res.data)
+            setSiteUrl(res.data.siteurl.option_value)
+            setHomeUrl(res.data.home.option_value)
+            setAminUrl(res.data.admin_home.option_value)
+            setSiteName(res.data.sitename.option_value)
+            setSiteDesc(res.data.sitedescription.option_value)
+        })
+    }, [])
+
+    const saveSettings = () => {
+        const key = 'updatable';
+        message.loading({ content: 'Loading...', key });
+        axios({
+            method: 'post',
+            url: `/admin/api/global/save`,
+            headers: {
+                'Authorization': 'Bearer ' + Cookies.get('token'),
+            },
+            data: {
+                siteurl: siteUrl,
+                home: homeUrl,
+                admin_home: aminUrl,
+                sitename: siteName,
+                sitedescription: siteDesc,
+            }
+        }).then(res => {
+            if (!res.data.error) {
+                message.success({ content: res.data.message, key, duration: 2 });
+            } else {
+
+            }
+        })
+    }
+
     return (
         <Card>
-            Site url: <Input />
-            Home url: <Input />
-            Admin home url: <Input />
-            Site Name: <Input />
-            Site description: <TextArea />
+            <div>
+                Site url: <Input value={siteUrl} onChange={(e) => { setSiteUrl(e.target.value); setSave(true) }} />
+            Home url: <Input value={homeUrl} onChange={(e) => { setHomeUrl(e.target.value); setSave(true) }} />
+            Admin url: <Input value={aminUrl} onChange={(e) => { setAminUrl(e.target.value); setSave(true) }} />
+            Site Name: <Input value={siteName} onChange={(e) => { setSiteName(e.target.value); setSave(true) }} />
+            Site description: <TextArea value={siteDesc} onChange={(e) => { setSiteDesc(e.target.value); setSave(true) }} />
+            </div>
+            {save && <Button onClick={saveSettings} >Save</Button>}
         </Card>
     )
 }
