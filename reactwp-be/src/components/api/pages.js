3e6f43e707e97595b74e505a495a131b42d64e43
Illia <syxou27@gmail.com>
2020-05-22 19:15:36 +0200

    add endpoints for the site (API)

---
+++ reactwp-be/src/components/api/pages.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+
+const Post = require('../../models/post')
+
+router.get('/all', async (req, res, next) => {
+    try {
+        const posts = await Post.query()
+            .where('type', 'page')
+        res.json(posts)
+    } catch (error) {
+        console.log(error)
+        next()
+    }
+})
+router.get('/:id', async (req, res, next) => {
+    const id = req.params.id
+    try {
+        const posts = await Post.query()
+            .where('type', 'page')
+            .where('id', id)
+        res.json(posts)
+    } catch (error) {
+        console.log(error)
+        next()
+    }
+})
+
+getPostByid = (type, id) => {
+
+}
+
+router.get('/:id/data', async (req, res, next) => {
+    const id = req.params.id
+    try {
+        const post = await Post.query().findById(id).where('type', 'page')
+
+        const fields = await post.$relatedQuery('fields').where('post_id', id)
+        res.json({ fields: fields, post: post })
+    } catch (error) {
+        console.log(error)
+        next()
+    }
+})
+
+module.exports = router
--- reactwp-be/src/routes/api.js
+++ reactwp-be/src/routes/api.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
+const pages = require('../components/api/pages')
+
+/**
+ * /api...
+ */
 router.get('/', function (req, res, next) {
-    res.send('api now then');
+    res.json({
+        get_app_post: {
+            all_get: '/api/page/all',
+            byid_get: '/api/page/:id',
+            page_data: '/api/page/:id/data',
+        }
+    });
 });
+
+router.use('/page', pages)
 
 module.exports = router;
