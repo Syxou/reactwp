9f226eb7f38e5322371fe71ef0d950edf2c9a06d
Illia <syxou27@gmail.com>
2020-06-12 15:17:12 +0200

    Add new fichiers in listing

--- reactwp-fe/src/compontnts/listing/Listing.js
+++ reactwp-fe/src/compontnts/listing/Listing.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { Button } from 'antd'
 import './Listing.css'
 
 const Card = styled.div`
     display: flex;
-    padding: 10px;
-    border-bottom: solid 1px #EBEDF4;
+    padding: 10px 15px;
     align-items: center;
+    margin: 15px 0;
+    background-color: #ffffff;
+    justify-content:space-between;
+    box-shadow: 0px 10px 20px rgba(31, 32, 65, 0.05);
+    border-radius: 5px;
+    width: inherit;
+    height: max-content;
+    transition: all 0.3s ease;
+    animation-duration: 0.3s;
+    animation-name: slidein;
+
     img{
         object-fit: cover;
         width: 40px;
@@ -31,18 +42,34 @@ const Card = styled.div`
 `
 
 
-class Listing extends Component {
-    render() {
-        return (
-            <Card>
-                <img src={this.props.image} alt="" />
-                <div>
-                    <p>{this.props.name}</p>
-                    <span>{this.props.status}</span>
-                </div>
-                <Link to={this.props.link}>{this.props.textLink}</Link>
-            </Card >
-        );
-    }
+const Listing = (props) => {
+    return (
+        <Card>
+            {props.image && <img src={props.image} alt="" />}
+            <div>
+                <p>{props.name}</p>
+                <span>{props.status}</span>
+            </div>
+            <div>
+                <Link to={props.link}>
+                    <Button
+                        type="dashed"
+                        style={{ borderColor: "#1890ff"}}
+                    >
+                        {props.textLink}
+                    </Button>
+                </Link>
+                {props.callbackRemove &&
+                    <Button
+                        type="dashed"
+                        shape="circle"
+                        icon="delete"
+                        style={{ borderColor: "tomato", margin: ' 0 5px' }}
+                        onClick={() => props.callbackRemove(props.id)}
+                    />
+                }
+            </div>
+        </Card >
+    );
 }
 export default Listing
